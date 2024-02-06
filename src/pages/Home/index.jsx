a002a9d38bd6c5cfed9a981c6402d39207d52246
ddevitz <danieladevitz@gmail.com>
2024-02-05 22:50:00 -0300

    Starting to create the Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
-import { Container } from "./styles";
+import { Container, Top, Section, Slider } from "./styles";
+
+import macaronsImg from '../../assets/pngegg 1.png';
 
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
@@ -9,7 +11,21 @@ export function Home(){
         <Container>
             <Header />
 
-            <FoodCard />
+            <Top>
+                <img src={macaronsImg} alt="" />
+
+                <div className="description">
+                    <h1>Sabores inigualáveis</h1>
+                    <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
+                </div>
+            </Top>
+
+            <Section>
+                <h2>Refeições</h2>
+                <Slider>
+                    <FoodCard />
+                </Slider>
+            </Section>
             
             <Footer />
         </Container>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -8,7 +8,66 @@ export const Container = styled.div`
     grid-template-rows: 100px auto 80px;
     grid-template-areas: 
     "header"
+    "top"
     "content"
     "footer"
     ;
+    
 `;
+
+export const Top = styled.div`
+    grid-area: top;
+
+    background: linear-gradient(to bottom, #091E26, #00131C);
+
+    width: 1120px;
+    height: 260px;
+
+    display: flex;
+    justify-content: space-between;
+
+    border-radius: 8px;
+    margin: 164px auto;
+
+    .description{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        margin-right: 100px;
+    }
+
+    > img {
+        width: 656px;
+        height: 412px;
+        box-sizing: content-box;
+
+        position: relative;
+        bottom: 152px;
+    }
+
+    h1{
+        font-weight: 400;
+    }
+
+    p {
+        font-size: 12px;
+    }
+`;
+
+export const Section = styled.div`
+    grid-area: content;
+    
+    width: 100%;
+    height: 100vh;
+
+    h2 {
+        font-weight: 400;
+    }
+`;
+
+export const Slider = styled.div`
+
+`;
+
